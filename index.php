3c042e8391329b13e482cbf392e97e6aff60091d
Rodrigo Teixeira <drigoteixeira@gmail.com>
2015-12-16 22:27:07 -0200

    Adding new files to repo

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<!--[if IE 7]> <html class="no-js ie7" lang="pt-br"> <![endif]-->
+<!--[if IE 8]> <html class="no-js ie8" lang="pt-br"> <![endif]-->
+<!--[if IE 9]> <html class="no-js ie9" lang="pt-br"> <![endif]-->
+<head>
+    <meta charset="utf-8">
+    <title>Hooked</title>
+    <?php include __DIR__ . '/build/in/meta.php'; ?>
+    <?php include __DIR__ . '/build/in/estilos.php'; ?>
+</head>
+<body>
+    <!-- Header -->
+    <?php include __DIR__ . '/build/in/header.php'; ?>
+
+    <main role="main">
+
+    </main>
+
+    <!-- Footer -->
+    <?php include __DIR__ . '/build/in/footer.php'; ?>
+
+    <!-- Scripts -->
+    <script src="build/js/scripts.min.js"></script>
+</body>
+</html>
---
+++ indice.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<!--[if IE 7]> <html class="no-js ie7" lang="pt-br"> <![endif]-->
+<!--[if IE 8]> <html class="no-js ie8" lang="pt-br"> <![endif]-->
+<!--[if IE 9]> <html class="no-js ie9" lang="pt-br"> <![endif]-->
+<head>
+    <meta charset="utf-8">
+    <title>Templates | Hooked</title>
+    <?php include __DIR__ . '/build/in/meta.php'; ?>
+    <?php include __DIR__ . '/build/in/estilos.php'; ?>
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Índice de templates</h1>
+
+
+                <table class="table table-striped table-responsive">
+                    <thead>
+                        <tr>
+                            <th>Nome da Página</th>
+                            <th>HTML</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>0. Página inicial</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>1. Termos e Serviços</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>2. Política de Privacidade</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>3. FAQ</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>4. Contato</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>5. Categoria de Produtos</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>– 5.1 Página do Produto</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>6. Login</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>7. Loja</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>8. Resultado da Busca</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>– 8.1 Busca Vazia</td>
+                            <td>-</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
