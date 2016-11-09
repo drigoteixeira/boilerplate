4fcaed1fa4e55769fbc0ee9428a30acefc30a57b
Rodrigo Teixeira <rodrigo@a2comunicacao.com.br>
2016-11-09 17:43:08 -0200

    Update news, faq and content-sidebar templates

---
+++ content-sidebar.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<!--[if IE 7]> <html class="no-js ie7" lang="pt-br"> <![endif]-->
+<!--[if IE 8]> <html class="no-js ie8" lang="pt-br"> <![endif]-->
+<!--[if IE 9]> <html class="no-js ie9" lang="pt-br"> <![endif]-->
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>Content with Sidebar | UIKIT</title>
+    <?php include "build/in/meta.php"; ?>
+    <?php include "build/in/estilos.php"; ?>
+</head>
+<body>
+    <!-- Header -->
+    <?php include "build/in/header.php"; ?>
+    <main class="main">
+       
+        <ol class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Library</a></li>
+            <li class="active">Data</li>
+        </ol>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 page-header">
+                   <h1>Page</h1>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-2">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li><a href="#">Item 1</a></li>
+                        <li><a href="#">Item 2</a></li>
+                        <li><a href="#">Item 3</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-10">
+                   
+                    <p>Although, today we’re even more excited to release our Wireframe Kit. The name speaks for itself and how you could’ve already guessed, Wireframe Kit is a perfect instrument for creating serious prototypes and complex web projects. Basically, the kit consists of more than a hundred ready-to-use elements that you can combine to get the exact prototype you want. We tried to make it as versatile as possible, at the same time getting rid of all the redundant features to help you save time.</p>
+                    
+                    <div class="page-header">
+                        <h3>A whole lot of inspiration you can get from the vast variety of combinations</h3>
+                    </div>
+                    
+                    <p>Wireframe Kit is a professional instrument; here you won’t find any bright interface or colorful layout, only a precise set of tools to make a working prototype of any complexity. So, why don’t we summarize all we have here: a set of 130 elements, perfectly fitting each other. Click, drag, resize, adapt, combine, just like that. An excellent way of preparing a project’s wireframe and presenting it to the customer before thinking of the graphic design.</p>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- Footer -->
+    <?php include "build/in/footer.php"; ?>
+
+</body>
+</html>
+
---
+++ news.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<!--[if IE 7]> <html class="no-js ie7" lang="pt-br"> <![endif]-->
+<!--[if IE 8]> <html class="no-js ie8" lang="pt-br"> <![endif]-->
+<!--[if IE 9]> <html class="no-js ie9" lang="pt-br"> <![endif]-->
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>Content | UIKIT</title>
+    <?php include "build/in/meta.php"; ?>
+    <?php include "build/in/estilos.php"; ?>
+</head>
+<body>
+    <!-- Header -->
+    <?php include "build/in/header.php"; ?>
+
+    <main class="main">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-8">
+                  
+                    <div class="page-header">
+                        <h1>This is a great time do build a Wireframe</h1>
+                        <p class="lead">This is a grat subheader here. Let's make it cool!</p>
+                    </div>
+                   
+                    <p>Although, today we’re even more excited to release our Wireframe Kit. The name speaks for itself and how you could’ve already guessed, Wireframe Kit is a perfect instrument for creating serious prototypes and complex web projects. Basically, the kit consists of more than a hundred ready-to-use elements that you can combine to get the exact prototype you want. We tried to make it as versatile as possible, at the same time getting rid of all the redundant features to help you save time.</p>
+                
+                    
+                    <p>Wireframe Kit is a professional instrument; here you won’t find any bright interface or colorful layout, only a precise set of tools to make a working prototype of any complexity. So, why don’t we summarize all we have here: a set of 130 elements, perfectly fitting each other. Click, drag, resize, adapt, combine, just like that. An excellent way of preparing a project’s wireframe and presenting it to the customer before thinking of the graphic design.</p>
+                    
+                    <p>Although, today we’re even more excited to release our Wireframe Kit. The name speaks for itself and how you could’ve already guessed, Wireframe Kit is a perfect instrument for creating serious prototypes and complex web projects. Basically, the kit consists of more than a hundred ready-to-use elements that you can combine to get the exact prototype you want. We tried to make it as versatile as possible, at the same time getting rid of all the redundant features to help you save time.</p>
+                
+                    
+                    <p>Wireframe Kit is a professional instrument; here you won’t find any bright interface or colorful layout, only a precise set of tools to make a working prototype of any complexity. So, why don’t we summarize all we have here: a set of 130 elements, perfectly fitting each other. Click, drag, resize, adapt, combine, just like that. An excellent way of preparing a project’s wireframe and presenting it to the customer before thinking of the graphic design.</p>
+                    
+                    <p>Although, today we’re even more excited to release our Wireframe Kit. The name speaks for itself and how you could’ve already guessed, Wireframe Kit is a perfect instrument for creating serious prototypes and complex web projects. Basically, the kit consists of more than a hundred ready-to-use elements that you can combine to get the exact prototype you want. We tried to make it as versatile as possible, at the same time getting rid of all the redundant features to help you save time.</p>
+                
+                    
+                    <p>Wireframe Kit is a professional instrument; here you won’t find any bright interface or colorful layout, only a precise set of tools to make a working prototype of any complexity. So, why don’t we summarize all we have here: a set of 130 elements, perfectly fitting each other. Click, drag, resize, adapt, combine, just like that. An excellent way of preparing a project’s wireframe and presenting it to the customer before thinking of the graphic design.</p>
+                    
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- Footer -->
+    <?php include "build/in/footer.php"; ?>
+
+</body>
+</html>
+
